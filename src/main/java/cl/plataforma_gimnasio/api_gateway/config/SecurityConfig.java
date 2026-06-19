0b9b8da3cf30c14d9e4388e21b3ce9545f6f8b11
ji-munoza <ji.munoza@duocuc.cl>
2026-06-19 04:11:48 -0400

    desactivada seguridad

--- src/main/java/cl/plataforma_gimnasio/api_gateway/config/SecurityConfig.java
+++ src/main/java/cl/plataforma_gimnasio/api_gateway/config/SecurityConfig.java
@@ -3,105 +3,25 @@ package cl.plataforma_gimnasio.api_gateway.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
-import org.springframework.web.server.WebFilter;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtUtils jwtUtils;
-
     @Bean
-    public SecurityWebFilterChain swaggerSecurityFilterChain(ServerHttpSecurity http) {
-        return http
-                .securityMatcher(ServerWebExchangeMatchers.pathMatchers(
-                        "/doc/swagger-ui.html",
-                        "/doc/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/webjars/**",
-                        "/favicon.ico",
-                        "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"
-                ))
-
-                .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll())
-                .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable())
-                .build();
-    }
-
-    @Bean
-    public SecurityWebFilterChain apiSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(csrf -> csrf.disable())
-                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
-
-                .exceptionHandling(exception -> exception
-                        .authenticationEntryPoint((exchange, e) -> {
-                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                            return exchange.getResponse().setComplete();
-                        })
-                )
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/api/gimnasio/auth/**").permitAll()
-                        .pathMatchers("/api/gimnasio/socios/**").hasAuthority("ROLE_ADMIN")
-                        .pathMatchers("/api/gimnasio/evaluaciones/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_ENTRENADOR")
-                        .anyExchange().authenticated()
-                )
-                .addFilterAt(jwtAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
+                        .anyExchange().permitAll()
+                );
 
         return http.build();
-    }
-
-    private WebFilter jwtAuthenticationFilter() {
-        return (exchange, chain) -> {
-            String path = exchange.getRequest().getURI().getPath();
-
-            if (path.contains("/doc") || path.contains("swagger") || path.contains("api-docs") || path.contains("/webjars")) {
-                return chain.filter(exchange);
-            }
-
-            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-
-            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                String token = authHeader.substring(7);
-                if (jwtUtils.validateToken(token)) {
-                    String username = jwtUtils.extractUsername(token);
-                    List<String> roles = jwtUtils.extractRoles(token);
-
-                    List<SimpleGrantedAuthority> authorities = roles.stream()
-                            .map(SimpleGrantedAuthority::new)
-                            .collect(Collectors.toList());
-
-                    UsernamePasswordAuthenticationToken authentication =
-                            new UsernamePasswordAuthenticationToken(username, null, authorities);
-
-                    SecurityContextImpl context = new SecurityContextImpl(authentication);
-
-                    return chain.filter(exchange)
-                            .contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(context)));
-                }
-            }
-            return chain.filter(exchange);
-        };
     }
 }
